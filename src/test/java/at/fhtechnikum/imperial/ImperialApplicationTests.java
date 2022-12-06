3221f1acbe3f0dfb525806742ddae178334dcd19
Djihan <ic20b020@technikum-wien.at>
2022-12-06 20:57:34 +0100

    #3 add tests for the calculations

--- src/test/java/at/fhtechnikum/imperial/ImperialApplicationTests.java
+++ src/test/java/at/fhtechnikum/imperial/ImperialApplicationTests.java
@@ -1,13 +1,27 @@
 package main.java.at.fhtechnikum.imperial;
 
+import main.java.at.fhtechnikum.imperial.Controller.ImperialController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ImperialApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		// Vorhanden
+		ImperialController controller = new ImperialController();
+
+		// Falls
+		int resultInch = controller.calcInch(1);
+		int resultYard = controller.calcYard(1);
+
+		// Sollte ergebniss
+		assertEquals(resultInch, 2.54);
+		assertEquals(resultYard, 0.9144);
+
 	}
 
 }
